b35f111b125741702b4459da16fefcfd62c09777
James Conway <35849960+615283@users.noreply.github.com>
2019-05-03 15:26:06 +0100

    basement wall block and group

--- src/main/java/uk/co/xsc/TestMod.java
+++ src/main/java/uk/co/xsc/TestMod.java
@@ -1,11 +1,10 @@
 package uk.co.xsc;
 
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.client.itemgroup.FabricItemGroupBuilder;
 import net.minecraft.block.Block;
 import net.minecraft.block.Material;
-import net.minecraft.item.BlockItem;
-import net.minecraft.item.Item;
-import net.minecraft.item.ItemGroup;
+import net.minecraft.item.*;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 import uk.co.xsc.blocks.BasementWallBlock;
@@ -15,17 +14,20 @@ import uk.co.xsc.sounds.GreetingGlobglogabgolabSound;
 @SuppressWarnings("WeakerAccess")
 public class TestMod implements ModInitializer {
 
-    public static final GlobglogabgolabItem GLOBGLOGABGOLAB_ITEM = new GlobglogabgolabItem(new Item.Settings().itemGroup(ItemGroup.COMBAT));
+    public static final ItemGroup GLOBGLOGABGOLAB_GROUP = FabricItemGroupBuilder.create(new Identifier("test_mod", "globglogabgolab")).icon(() -> new ItemStack(Items.ACACIA_PLANKS)).build();
+
+    public static final GlobglogabgolabItem GLOBGLOGABGOLAB_ITEM = new GlobglogabgolabItem(new Item.Settings().itemGroup(GLOBGLOGABGOLAB_GROUP));
 
     public static final BasementWallBlock BASEMENT_WALL_BLOCK = new BasementWallBlock(Block.Settings.of(Material.STONE).friction(50));
 
     public static final GreetingGlobglogabgolabSound GREETING_GLOBGLOGABGOLAB_SOUND = new GreetingGlobglogabgolabSound(new Identifier("test_mod", "greeting_globglogabgolab_sound"));
 
+
     @Override
     public void onInitialize() {
         Registry.register(Registry.BLOCK, new Identifier("test_mod", "basement_wall_block"), BASEMENT_WALL_BLOCK);
 
-        Registry.register(Registry.ITEM, new Identifier("test_mod", "basement_wall_block"), new BlockItem(BASEMENT_WALL_BLOCK, new Item.Settings().stackSize(128).itemGroup(ItemGroup.COMBAT)));
+        Registry.register(Registry.ITEM, new Identifier("test_mod", "basement_wall_block"), new BlockItem(BASEMENT_WALL_BLOCK, new Item.Settings().stackSize(128).itemGroup(GLOBGLOGABGOLAB_GROUP)));
 
         Registry.register(Registry.ITEM, new Identifier("test_mod", "globglogabgolab_item"), GLOBGLOGABGOLAB_ITEM);
 
